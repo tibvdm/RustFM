9165dc8f2b41a268037c03181b7ba65c816171df
tibvdm <tibovandemoortele@hotmail.com>
2022-06-25 12:47:35 +0200

    index operator and gitignore

--- src/bitvector.rs
+++ src/bitvector.rs
@@ -1,3 +1,7 @@
+use std::ops::Index;
+
+const 1L: u64 = 1;
+
 /// Bitvector with Jacobson’s rank
 pub struct Bitvec {
     /// Size of the bitvector
@@ -8,4 +12,14 @@ pub struct Bitvec {
 
     /// Interleaved first and second level counts
     counts: Vec<u64>
+}
+
+impl Index<usize> for Bitvec {
+    type Output = bool;
+
+    fn index(&self, pos: usize) -> &Self::Output {
+        usize word = pos / 64;
+        usize bit  = pos % 64;
+        return bitvector[word] & (1L << bit)
+    }
 }
