1c31610284945ba3d6c6e4bae2798fd3b6473c58
tibvdm <tibovandemoortele@hotmail.com>
2022-06-29 15:03:42 +0200

    remove binary reader from main

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,9 @@ use std::fs::File;
 
 use rust_fm::errors::Result;
 use rust_fm::alphabet::DNAAlphabet;
-use rust_fm::io::alphabet_reader::AlphabetReader;
 
 fn main() -> Result<()> {
     let mut file = File::open("./data/a.text")?;
-
-    let mut reader = AlphabetReader::new(file, DNAAlphabet::default());
-
-    if let Some(x) = reader.read_character()? {
-        println!("{}", x);
-    }
     
     Ok(())
 }
