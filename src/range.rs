4f0118ce89cc2820a21bd3b44108436d1aa17480
tibvdm <tibovandemoortele@hotmail.com>
2022-07-08 00:19:19 +0200

    range and tree tests

--- src/range.rs
+++ src/range.rs
@@ -1,7 +1,7 @@
 use std::cmp::PartialOrd;
 
 /// Represents a range [start, end[
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub struct Range<T: PartialOrd> {
     /// Start value of the range
     pub start: T,
@@ -20,5 +20,28 @@ impl<T: PartialOrd> Range<T> {
 
     pub fn empty(&self) -> bool {
         return self.end <= self.start;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::range::Range;
+
+    #[test]
+    fn test_range_new() {
+        let range = Range::new(0, 5);
+
+        assert_eq!(range.start, 0);
+        assert_eq!(range.end, 5);
+        assert_eq!(range.empty(), false);
+    }
+
+    #[test]
+    fn test_range_empty() {
+        let range = Range::new(0, 0);
+
+        assert_eq!(range.start, 0);
+        assert_eq!(range.end, 0);
+        assert_eq!(range.empty(), true);
     }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -7,6 +7,7 @@ use crate::{
     range::Range
 };
 
+#[derive(Debug)]
 pub struct Position {
     /// Range over the suffix array
     range: Range<usize>,
@@ -37,6 +38,12 @@ impl Position {
 
     pub fn row(&self) -> usize {
         return self.depth;
+    }
+}
+
+impl PartialEq for Position {
+    fn eq(&self, other: &Self) -> bool {
+        return self.range == other.range;
     }
 }
 
@@ -74,5 +81,87 @@ impl<'a, A: Alphabet> Iterator for SearchTree<'a, A> {
 
     fn next(&mut self) -> Option<Position> {
         return self.search_space.pop();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        alphabet::{
+            Alphabet,
+            AlphabetChar,
+            AlphabetIndex,
+            DNAAlphabet
+        },
+        tree::{
+            FMIndex,
+            Position,
+            Range,
+            SearchTree
+        }
+    };
+
+    const INPUT_VEC: [AlphabetChar; 20] = [
+        b'A', b'A', b'C', b'T', b'A', b'G', b'G', b'G', b'C', b'A', b'A', b'T', b'G', b'T', b'T',
+        b'C', b'A', b'A', b'C', b'G'
+    ];
+
+    #[test]
+    fn test_position_new() {
+        let position = Position::new(Range::new(0, 5), 0, 0);
+
+        assert_eq!(position.range().start, 0);
+        assert_eq!(position.range().end, 5);
+        assert_eq!(position.character(), 0);
+        assert_eq!(position.row(), 0);
+    }
+
+    #[test]
+    fn test_search_tree_new() {
+        let fm_index = FMIndex::new(INPUT_VEC.to_vec(), DNAAlphabet::default(), 1);
+
+        let search_tree = SearchTree::new(&fm_index);
+
+        assert_eq!(search_tree.search_space, vec![]);
+    }
+
+    #[test]
+    fn test_extend_search_space() {
+        let fm_index = FMIndex::new(INPUT_VEC.to_vec(), DNAAlphabet::default(), 1);
+
+        let mut search_tree = SearchTree::new(&fm_index);
+
+        let extend_results = vec![(1, 8), (8, 12), (12, 16), (17, 21)];
+
+        search_tree.extend_search_space(&Range::new(0, 20), 0);
+
+        assert_eq!(search_tree.search_space.len(), 4);
+
+        for i in 0 .. fm_index.alphabet().len() {
+            assert_eq!(search_tree.search_space[i].range().start, extend_results[i].0);
+            assert_eq!(search_tree.search_space[i].range().end, extend_results[i].1);
+            assert_eq!(search_tree.search_space[i].character(), i as AlphabetIndex);
+            assert_eq!(search_tree.search_space[i].row(), 1);
+        }
+    }
+
+    #[test]
+    fn test_iterator() {
+        let fm_index = FMIndex::new(INPUT_VEC.to_vec(), DNAAlphabet::default(), 1);
+
+        let mut search_tree = SearchTree::new(&fm_index);
+
+        search_tree.extend_search_space(&Range::new(0, 20), 0);
+
+        let mut i = 0;
+        while let Some(_) = search_tree.next() {
+            if i < 4 {
+                search_tree.extend_search_space(&Range::new(0, 20), 0);
+            }
+
+            i += 1;
+        }
+
+        assert_eq!(i, 20);
     }
 }
